441439bec4ee7e52e85e208ee760332b6c5c7566
Nikola Smiljanic <nikolasmiljanic65@gmail.com>
2024-12-22 17:20:49 +0100

    controller changed

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,12 +3,7 @@ import { Controller, Get } from '@nestjs/common';
 @Controller()
 export class AppController {
   @Get()
-  getHello(): string {
-    return 'Hello World';
-  }
-
-  @Get('world')
-  getWorld(): string {
-    return 'World!!!';
+  getIndex(): string {
+    return 'Home page!';
   }
 }
